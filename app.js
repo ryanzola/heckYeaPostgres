6df0e2eb99491b2e1732c5486884462b6e685f07
ryanzola <ryanzola@me.com>
2018-03-24 18:52:51 -0400

    This app is total CRUD! Full CRUD functionality, baby!

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cons = require('consolidate');
 const dust = require('dustjs-helpers');
-const { Pool, Client } = require('pg')
+const { Client } = require('pg');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Database connection string
+// Database connection
 const client = new Client({
   user: process.env.DB_USER,
   host: 'localhost',
   database: 'recipiebookdb',
   password: process.env.DB_PASSWORD,
-  port: 5433,
-})
+  port: 5433
+});
 client.connect();
-
 
 // Assign Dust engine to .dust files
 app.engine('dust', cons.dust);
@@ -34,12 +33,50 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
+  client.query('SELECT * FROM recipes', (err, result) => {
+    res.render('index', { recipes: result.rows });
+  });
+});
 
-  client.query('SELECT * FROM recipies', (err, result) => {
-    res.render('index', {recipies: result.rows})
-    client.end()
-  })
+app.post('/add', (req, res, next) => {
+  client.query(
+    'INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)',
+    [req.body.name, req.body.ingredients, req.body.directions],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.redirect('/', 400);
+      } else {
+        res.redirect('/', 201);
+      }
+    }
+  );
+});
+
+app.post('/edit', (req, res, next) => {
+  client.query(
+    'UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4',
+    [req.body.name, req.body.ingredients, req.body.directions, req.body.id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.redirect('/');
+      } else {
+        res.redirect('/');
+      }
+    }
+  );
+});
+
+app.delete('/delete/:id', (req, res, next) => {
+  client.query(
+    'DELETE FROM recipes WHERE id = $1',
+    [req.params.id],
+    (err, result) => {
+      res.sendStatus(200);
+    }
+  );
 });
 
 app.listen(port, () => {
