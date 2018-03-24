b7fc3edb06bac42655c328a9475c83330cbcaaef
ryanzola <ryanzola@me.com>
2018-03-24 03:29:20 -0400

    success!

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cons = require('consolidate');
+const dust = require('dustjs-helpers');
+const { Pool, Client } = require('pg')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Database connection string
+const client = new Client({
+  user: process.env.DB_USER,
+  host: 'localhost',
+  database: 'recipiebookdb',
+  password: process.env.DB_PASSWORD,
+  port: 5433,
+})
+client.connect();
+
+
+// Assign Dust engine to .dust files
+app.engine('dust', cons.dust);
+
+// Set default ext .dust
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+// Set public folder
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+
+  client.query('SELECT * FROM recipies', (err, result) => {
+    res.render('index', {recipies: result.rows})
+    client.end()
+  })
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
